76562f2531c47f80a55a9e079ceba12fae4202ba
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-11-03 15:41:39 +0100

    dichiarazione classe lavatrice

--- Program.cs
+++ Program.cs
@@ -18,100 +18,70 @@ Si richiede di creare un sistema di controllo in grado di riportare lo stato del
 */
 
 // Lavanderia
-    
-    //metodi:
-        // 1. lista che stampa quali macchine sono in funzione o no
-        // 2. incasso generato dalle macchine
+
+//metodi:
+// 1. lista che stampa quali macchine sono in funzione o no
+// 2. incasso generato dalle macchine
 
 // Lavarici
-    //caratteristiche:
-        //marca = string
-        //accesa = bool
-        //tipoDiLavaggio = string
-        //qtyDetersivo = int
-        //qtyAmmorbidente = int
-        //durataLavaggio = int
-        //tempoRimanenteLavaggio = int
+//caratteristiche:
+//marca = string
+//accesa = bool
+//tipoDiLavaggio = string
+//qtyDetersivo = int
+//qtyAmmorbidente = int
+//durataLavaggio = int
+//tempoRimanenteLavaggio = int
 
-    // programmi di lavaggio:
-        // 1.Rinfrescante   2gettoni, 20min, 20ml detersivo, 5ml ammorbidente
-        // 2.Rinnovante     3gettoni, 40min, 40ml detersivo, 10ml ammorbidente 
-        // 3.Sgrassante     4gettoni, 60min, 60ml detersivo, 15ml ammorbidente
+// programmi di lavaggio:
+// 1.Rinfrescante   2gettoni, 20min, 20ml detersivo, 5ml ammorbidente
+// 2.Rinnovante     3gettoni, 40min, 40ml detersivo, 10ml ammorbidente 
+// 3.Sgrassante     4gettoni, 60min, 60ml detersivo, 15ml ammorbidente
 
 // Asciugatrici
-    //caratteristiche:
-        //marca = string
-        //accesa = bool
-        //tipoDiAsciugatura = string
-        //durataAsciugatura = int
-        //tempoRimanenteAsciugatura = int
+//caratteristiche:
+//marca = string
+//accesa = bool
+//tipoDiAsciugatura = string
+//durataAsciugatura = int
+//tempoRimanenteAsciugatura = int
 
-    // programmi di asciugatura:
-        // 1.Rapido     2gettoni, 30min
-        // 2.Intenso    3gettoni, 60min
-
-//Inizializzazione lavanderia
-Lavanderia lavanderia = new Lavanderia();
+// programmi di asciugatura:
+// 1.Rapido     2gettoni, 30min
+// 2.Intenso    3gettoni, 60min
 
 
-//PROGRAMMA PRINCIPALE
-Console.WriteLine("Benvenuto nella Lavanderia.");
 
-lavanderia.StampaElettrodomestici();
 
-public class Elettrodomestico
+public class Lavatrice
 {
-    public bool Acceso { get; set; }
-    public string Marca { get; set; }
-    public string Programma { get; set; }
-    public int DurataProgramma { get; set; }
-    public int DurataRimanente { get; set; }
-    public int QtyDetersivo { get; set; }
-    public int QtyAmmorbidente { get; set; }
+    public string Nome { get; set; }
+    public int Gettoni { get; set; }
+    public int Tempo { get; set; }
+    public int Detersivo { get; set; }
+    public int Ammorbidente { get; set; }
+    public string Stato { get; set; }
 
-    public Elettrodomestico(bool acceso, string marca)
+    public Lavatrice(string nome)
     {
-        this.Acceso = acceso;
-        this.Marca = marca;
-    }
-    public Elettrodomestico(bool acceso, string marca, int qtyDetersivo, int qtyAmmorbidente)
-    {
-        this.Acceso = false;
-        this.Marca = marca;
-        this.QtyDetersivo = qtyDetersivo;
-        this.QtyAmmorbidente = qtyAmmorbidente;
-    }
-}
-
-public class Lavanderia
-{
-    private Elettrodomestico[] elettrodomestici;
-    public Lavanderia()
-    {
-        //lavatrici
-        elettrodomestici = new Elettrodomestico[10];
-        elettrodomestici[0] = new Elettrodomestico(false, "Samsung lavatrice", 1000, 500);
-        elettrodomestici[1] = new Elettrodomestico(false, "Beko lavatrice", 1000, 500);
-        elettrodomestici[2] = new Elettrodomestico(false, "Lg lavatrice", 1000, 500);
-        elettrodomestici[3] = new Elettrodomestico(false, "Bosch lavatrice", 1000, 500);
-        elettrodomestici[4] = new Elettrodomestico(false, "Bosch lavatrice", 1000, 500);
-
-        //asciugatrici
-        elettrodomestici[5] = new Elettrodomestico(false, "Solarex asciugatrice");
-        elettrodomestici[6] = new Elettrodomestico(false, "Bosch asciugatrice");
-        elettrodomestici[7] = new Elettrodomestico(false, "Aqualtis asciugatrice");
-        elettrodomestici[8] = new Elettrodomestico(false, "Beko asciugatrice");
-        elettrodomestici[9] = new Elettrodomestico(false, "Electrolux asciugatrice");
+        Nome = nome;
+        Gettoni = 0;
+        Detersivo = 1000;
+        Ammorbidente = 500;
+        Stato = "vuota";
     }
 
-    public void StampaElettrodomestici()
+    // programmi lavaggi
+    public void Rinfrescante()
     {
-        Console.WriteLine("----- elettrodomestici -------");
-
-        for(int i = 0; i < elettrodomestici.Length; i++)
-        {
-            Console.WriteLine("{0} - {1}, {2}", (i + 1), elettrodomestici[i].Acceso, elettrodomestici[i].Marca);
-        }
+        Stato = "Rinfrescante in esecuzione";
+        Tempo = 20;
+        Gettoni += 2;
+        Detersivo -= 20;
+        Ammorbidente -= 5;
     }
+
+    
+
     
 }
